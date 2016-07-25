e4b0e58a7238c3f6d9c80da1e1d42fcb3b62da20
Trinimar Acevedo <datrymar@gmail.com>
2016-07-25 15:21:28 -0400

    Modify puma.rb for rails_env = production

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,19 @@
-# Change to match your CPU core count
-workers 2
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-# Min and Max threads per worker
-threads 1, 6
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+#environment ENV['RACK_ENV'] || 'development'
+
+#on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+#end
 
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
@@ -12,7 +23,7 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{shared_dir}/tmp/sockets/sample_app-puma.sock"
 
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
